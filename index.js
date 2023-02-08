0bf94617c44751b36296ec8f60b54f2ffa0261ed
lalith1919 <121842503+lalith1919@users.noreply.github.com>
2023-02-08 14:51:36 -0600

    CP-32412 | Cosnole Change

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ document.getElementById("day").innerHTML = "Today is: "  +  day;
 var today = new Date();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 console.log(time)
+console.log(time)
 
 
 
